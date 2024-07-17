cbdf3b4e8c002c2618bdf8716ef2c683dbdb5702
sahasuvankar7 <121181194+sahasuvankar7@users.noreply.github.com>
2024-07-17 20:07:41 +0530

    Update url.js

--- Frontend/src/url.js
+++ Frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL = "https://blog-sphere-applicationbackend-sahasuvankar7s-projects.vercel.app/";
-export const IF = "https://blog-sphere-applicationbackend-sahasuvankar7s-projects.vercel.app/images/"
+export const URL = "https://blog-sphere-applicationbackend-sahasuvankar7s-projects.vercel.app";
+export const IF = "https://blog-sphere-applicationbackend-sahasuvankar7s-projects.vercel.app/images"
